9311611d5e893474af5196b4e2fa0a5a1baee6cc
Christine Xiang <yutingcxiang@gmail.com>
2018-09-06 05:23:34 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,29 @@
 class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+  
+  def create
+    @genre = Genre.new(params(:name))
+    @genre.save
+    redirect_to genre_path(@genre)
+  end
+  
+  def show
+    @genre = Genre.find_by(id: params[:id])
+  end
+  
+  def edit
+    @genre = Genre.find_by(id: params[:id])
+  end
+  
+  def update
+    @genre = Genre.find_by(id: params[:id])
+    @genre.update(params(:name))
+    redirect_to genre_path(@genre)
+  end
+  
+  def params(*args)
+    params.require(:genre).permit(*args)
+  end
 end
